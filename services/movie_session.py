48e4dc58545cca2b0879cbef57a1b36cb56ee2de
Sofiia <sonagm9@gmail.com>
2024-05-03 14:10:37 +0300

    Solution5 fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,7 +9,7 @@ from db.models import MovieSession
 def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int) -> None:
-    return MovieSession.objects.create(
+    MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall_id=cinema_hall_id,
         movie_id=movie_id
